b24f074b6c803fe89a426634d7e655455f0b65bf
ardhptr21 <ardhiputrapradana21@gmail.com>
2022-02-16 12:09:57 +0700

    tailwindcss config add fonts and colors

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,9 @@ import type { NextPage } from 'next';
 
 const Home: NextPage = () => {
   return (
-    <div className="flex justify-center h-screen items-center bg-gray-100 text-slate-400 text-xl">Hello World</div>
+    <div className="flex text-8xl font-montserrat font-semibold justify-center h-screen items-center bg-gray-100 bg-gradient-to-tr from-pastel-purple via-pastel-indigo to-pastel-green">
+      Hello World
+    </div>
   );
 };
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,24 @@
+const colors = require('tailwindcss/colors');
+
 module.exports = {
   content: ['./pages/**/*.{ts,tsx}', './components/**/*.{ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'open-sans': ['"Open Sans"', 'sans-serif'],
+        montserrat: ['Montserrat', 'sans-serif'],
+      },
+      colors: {
+        pastel: {
+          green: colors.green['400'],
+          pink: colors.pink['500'],
+          blue: colors.blue['500'],
+          indigo: colors.indigo['400'],
+          purple: colors.indigo['500'],
+          yellow: colors.yellow['300'],
+        },
+      },
+    },
   },
   plugins: [],
 };
